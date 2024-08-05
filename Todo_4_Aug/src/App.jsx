950d92859f33c967635fb2b3757af26ff647d426
avadhut86 <avipitale2003@gmail.com>
2024-08-05 15:21:03 +0800

    New Todo

---
+++ Todo_4_Aug/src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import Button from './Button'
+import Popup from './Popup'
+import TodoData from './TodoData'
+
+function App() {
+  
+
+  return (
+    <>
+     <Button/>
+     <Popup/>
+     {/* <TodoData/>     */}
+    </>
+  )
+}
+
+export default App
---
+++ Todo_4_Aug/src/Popup.jsx
@@ -0,0 +1,28 @@
+import { DatePicker, LocalizationProvider } from '@mui/x-date-pickers'
+import dayjs from 'dayjs'
+import React from 'react'
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
+
+const Popup = () => {
+  return (
+    
+    <div className='.card2-container'>
+    <div className='card-2'>
+    {/* <p className='para2'>Date : Monday, 10:00 am 2024</p> */}
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
+      <DatePicker  value={dayjs('2024-08-04T08:31:18.462Z')} onChange={(e) => setselectDate(e.$d.toISOString())
+      }/>
+    </LocalizationProvider>
+    <hr />
+     
+     <div className='child'>
+    <input className='input' type="text"  placeholder='Please Enter Your Task Here' />
+    </div>
+    
+    <button className='addtask'>ADD TASK</button>
+</div>
+</div>
+  )
+}
+
+export default Popup
